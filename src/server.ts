4948b41e5e698ca1c5b8bb367778c33a52f44323
MariaMendess <mariaodetetenorio@gmail.com>
2022-01-11 21:31:57 -0300

    coding: Codficaçao do module cars e suas especificaçoes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { categoriesRouter } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/categories", categoriesRouter)
+app.use("/categories", categoriesRouter);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3000, () => console.log('Server on!'));
