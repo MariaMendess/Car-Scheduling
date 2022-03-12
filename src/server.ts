8a19fee3a1d1056d7254432b0a3106d68f23c524
MariaMendess <mariaodetetenorio@gmail.com>
2022-03-12 15:40:56 -0300

    coding: Implementaçao de dataBase

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import swaggerUI from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
@@ -12,4 +14,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(3000, () => console.log('Server on!'));
+app.listen(3333, () => console.log('Server on!'));
